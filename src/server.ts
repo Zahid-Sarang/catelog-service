b97be41b94b2c9cdcc192e75a85c98c587e52868
zahid-soft <zahidsarang95@gmail.com>
2023-12-29 22:06:22 +0530

    project-setup

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import app from "./app";
+import { Config } from "./config";
+import logger from "./config/logger";
+
+const startServer = () => {
+  const PORT = Config.PORT;
+  try {
+    app.listen(PORT, () => {
+      logger.info(`Listing on port ${PORT}`);
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      logger.error(error.message);
+      setTimeout(() => {
+        process.exit(1);
+      }, 1000);
+    }
+  }
+};
+
+startServer();
