6496a23738d8b8aa515cf9a80d822cb629bec1bd
zahid-soft <zahidsarang95@gmail.com>
2024-02-26 15:30:17 +0530

    add authorization

--- src/category/category-router.ts
+++ src/category/category-router.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import { Roles } from "../common/constants";
 import authenticate from "../common/middlewares/authenticate";
+import { canAccess } from "../common/middlewares/canAccess";
 import { asyncWrapper } from "../common/utils/ErrorWrapper";
 import logger from "../config/logger";
 import { CategoryController } from "./category-controller";
@@ -13,6 +15,7 @@ const categoryController = new CategoryController(categoryService, logger);
 categoryRouter.post(
     "/",
     authenticate,
+    canAccess([Roles.ADMIN]),
     categoryValidator,
     asyncWrapper(categoryController.create),
 );
