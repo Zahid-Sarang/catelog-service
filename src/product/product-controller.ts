7e5d77509b44f4412eb06a11a3bab6045fd7e262
zahid-soft <zahidsarang95@gmail.com>
2024-02-26 23:01:42 +0530

    create product

--- src/product/product-controller.ts
+++ src/product/product-controller.ts
@@ -1,13 +1,51 @@
 import { NextFunction, Response } from "express";
-import { Request } from "express-jwt";
 import { validationResult } from "express-validator";
 import createHttpError from "http-errors";
+import { Logger } from "winston";
+import { ProductService } from "./product-service";
+import { CreateProductRequest, Product } from "./product-types";
 
 export class ProductController {
-    async create(req: Request, res: Response, next: NextFunction) {
+    constructor(
+        private productService: ProductService,
+        private logger: Logger,
+    ) {}
+    create = async (
+        req: CreateProductRequest,
+        res: Response,
+        next: NextFunction,
+    ) => {
         const result = validationResult(req);
         if (!result.isEmpty()) {
             return next(createHttpError(400, result.array()[0].msg as string));
         }
-    }
+        const {
+            name,
+            description,
+            priceConfiguration,
+            attributes,
+            tenantId,
+            categoryId,
+            isPublish,
+        } = req.body;
+
+        const product = {
+            name,
+            description,
+            priceConfiguration: JSON.parse(priceConfiguration),
+            attributes: JSON.parse(attributes),
+            tenantId,
+            categoryId,
+            isPublish,
+            // todo: upload image
+            image: "image.jpg",
+        };
+
+        const newProduct = await this.productService.createProduct(
+            product as unknown as Product,
+        );
+        this.logger.info(`Created Product`, { id: newProduct._id });
+
+        res.json({ id: newProduct.id });
+    };
 }
--- src/product/product-router.ts
+++ src/product/product-router.ts
@@ -1,18 +1,22 @@
 import express from "express";
+import fileUpload from "express-fileupload";
 import { Roles } from "../common/constants";
 import authenticate from "../common/middlewares/authenticate";
 import { canAccess } from "../common/middlewares/canAccess";
 import { asyncWrapper } from "../common/utils/ErrorWrapper";
+import logger from "../config/logger";
 import createProductValidator from "./create-product-validator";
 import { ProductController } from "./product-controller";
+import { ProductService } from "./product-service";
 
 const productRouter = express.Router();
-
-const productController = new ProductController();
+const productService = new ProductService();
+const productController = new ProductController(productService, logger);
 productRouter.post(
     "/",
     authenticate,
     canAccess([Roles.ADMIN, Roles.MANAGER]),
+    fileUpload(),
     createProductValidator,
     asyncWrapper(productController.create),
 );
