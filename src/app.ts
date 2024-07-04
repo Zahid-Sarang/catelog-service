e93ef207b66b83d1d0b197debc8ed2d80e479928
zahid-soft <zahidsarang95@gmail.com>
2024-07-04 07:56:04 +0530

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import config from "config";
 import express, { Request, Response } from "express";
 import categoryRouter from "./category/category-router";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
@@ -6,8 +9,20 @@ import productRouter from "./product/product-router";
 import toppinRouter from "./topping/topping-router";
 
 const app = express();
+
+const ALLOWED_DOMAINS = [
+    config.get<string>("frontend.clientUI"),
+    config.get<string>("frontend.adminUI"),
+];
+
+app.use(
+    cors({
+        origin: ALLOWED_DOMAINS,
+    }),
+);
 app.use(express.json());
 app.use(cookieParser());
+
 app.get("/", (req: Request, res: Response) => {
     res.json({ message: "Hello from catalog service!" });
 });
